148d745967f30ce508837a88084d587b0c397914
NaosV1 <naos.contacts@gmail.com>
2026-03-25 13:56:27 +0100

    allowing only certain actions

--- server/server.py
+++ server/server.py
@@ -3,11 +3,18 @@ import threading
 
 def handle_client(conn, addr):
     print('nouvelle connexion')
+    allowed_actions = ["SEND_MESSAGE", "REQUEST_SALT", "JOIN_CHANNEL", "CREATE_CHANNEL"]
     while True:
         data = conn.recv(4096)
         if not data:
             break
         print(f"Données reçues : {data}")
+        if not data["action"] and data["data"]:
+            if data["action"] not in allowed_actions:
+                conn.sendall(json.dumps({"action": "ERROR", "data": "Invalid request"}).encode('utf-8'))
+                conn.close()
+            
+        
     conn.close()
     print('connexion fermée')
 
