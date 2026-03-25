c252f70817a4c05361485fa0ed79d945c8dfe126
KOSAAKU <kosaaku@gmail.com>
2026-03-25 10:51:41 +0100

    Define Function and Import

---
+++ client/crypto.py
@@ -0,0 +1,14 @@
+import os
+import hashlib
+import base64
+from cryptography.fernet import Fernet
+
+def hash_uid(uid):
+
+def generate_salt():
+
+def derive_key(uid, salt):
+
+def encrypt(message, key):
+
+def decrypt(message, key):
