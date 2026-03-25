96652530f2838858392c5e64067782e002aab430
KOSAAKU <kosaaku@gmail.com>
2026-03-25 11:15:11 +0100

    function hash_uid with commentary

--- client/crypto.py
+++ client/crypto.py
@@ -3,7 +3,10 @@ import hashlib
 import base64
 from cryptography.fernet import Fernet
 
-def hash_uid(uid):
+def hash_uid(uid): #Calcule et retourne le hash SHA-256 d'un UID sous forme de chaîne hexadécimale pour vérification d'intégrité.
+    h = hashlib.sha256() #Crée un objet SHA-256 vide pour le hachage 
+    h.update(uid.encode()) #Ajoute l'UID à l'objet hash pour commencer le SHA-256.
+    return h.hexdigest() #Retourne le hash SHA-256 final sous forme de chaîne hexadécimale lisible
 
 def generate_salt():
 
