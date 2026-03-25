ff3291a61af276c61d1de4899568f94ab6e21289
ifztrk <i.ozturk@ecole-ipssi.net>
2026-03-25 12:51:25 +0100

    communication serveur

--- Client/client.py
+++ Client/client.py
@@ -1,4 +1,22 @@
 import socket
-sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-sock.connect(('172.20.10.2', 8080))
+import threading
+import json
 
+def receive_messages(sock):
+    pass
+
+
+def main():
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    sock.connect(('172.20.10.2', 8080))
+    threading.Thread(target=receive_messages, args=(sock,)).start()
+    while True:
+        message = input()
+        payload = {
+            "action": "SEND_MESSAGE",
+            "data": message
+            }
+        sock.sendall(json.dumps(payload).encode('utf-8'))
+    sock.close()
+
+main()
