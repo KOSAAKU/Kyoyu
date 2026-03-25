a8939141dd4ee62a8554ef75fc524a3688abd527
KOSAAKU <kosaaku@gmail.com>
2026-03-25 12:02:16 +0100

    Salt generation + key creation w commentary

--- client/crypto.py
+++ client/crypto.py
@@ -9,8 +9,11 @@ def hash_uid(uid): #Calcule et retourne le hash SHA-256 d'un UID sous forme de c
     return h.hexdigest() #Retourne le hash SHA-256 final sous forme de chaîne hexadécimale lisible
 
 def generate_salt():
+    return os.urandom(16) #Génère un salt aléatoire de 16 octets
 
 def derive_key(uid, salt):
+    key = hashlib.pbkdf2_hmac('sha256', uid.encode(), salt, 100000) #transforme un UID faible en clé pratiquement incassable
+    return base64.urlsafe_b64encode(key).decode('utf-8') #Transforme les bytes de la clé en une chaîne base64 "URL-safe" (sans +//) lisible par Fernet.
 
 def encrypt(message, key):
 
