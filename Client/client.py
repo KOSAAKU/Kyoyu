44afda06ceeb39807fb926b61d9b19501c17790f
ifztrk <i.ozturk@ecole-ipssi.net>
2026-03-26 10:33:58 +0100

    recevoir et envoyer des messages marche enfin

--- Client/client.py
+++ Client/client.py
@@ -1,22 +1,66 @@
 import socket
 import threading
 import json
+from crypto import hash_uid, generate_salt, derive_key, encrypt, decrypt
 
-def receive_messages(sock):
-    pass
+def receive_messages(sock, key):
+    while True:
+        data = sock.recv(4096)
+        if not data:
+            break
+        payload = json.loads(data.decode())
+        action = payload.get("action")
+        
+        if action == "SEND_MESSAGE":
+            message = decrypt(payload["data"], key)
+            print(message)
 
 
 def main():
+    host = input("IP du serveur : ")
+    port = 8080
+    uid = input("UID du groupe : ")
+    
     sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    sock.connect(('172.20.10.2', 8080))
-    threading.Thread(target=receive_messages, args=(sock,)).start()
+    sock.connect((host, port))
+    
+    hashed_uid = hash_uid(uid)
+    
+    print("Que voulez vous faire ?")
+    print("1. Rejoindre un canal")
+    print("2. Créer un canal")
+    choix = input("Votre choix : ")
+    if choix == "1":
+
+        sock.sendall(json.dumps({"action": "REQUEST_SALT", "uid": hashed_uid}).encode())
+        response = json.loads(sock.recv(4096).decode())
+        if response["action"] == "ERROR":
+            print(f"Erreur : {response['data']}")
+            return
+        salt = response["data"]
+        sock.sendall(json.dumps({"action": "JOIN_CHANNEL", "uid": hashed_uid, "salt": salt}).encode())
+    elif choix == "2":
+        salt = generate_salt().hex()
+        sock.sendall(json.dumps({"action": "CREATE_CHANNEL", "uid": hashed_uid, "salt": salt}).encode())
+    else:
+        print("Choix invalide")
+        return
+    
+
+    
+    response = json.loads(sock.recv(4096).decode())
+    if response["status"] != "success":
+        print(f"Erreur : {response['message']}")
+        return
+    
+    key = derive_key(uid, bytes.fromhex(salt))
+    
+    threading.Thread(target=receive_messages, args=(sock, key), daemon=True).start()
+    
     while True:
         message = input()
-        payload = {
-            "action": "SEND_MESSAGE",
-            "data": message
-            }
-        sock.sendall(json.dumps(payload).encode('utf-8'))
-    sock.close()
+        sock.sendall(json.dumps({"action": "SEND_MESSAGE", "data": encrypt(message, key)}).encode())
+
 
 main()
+
